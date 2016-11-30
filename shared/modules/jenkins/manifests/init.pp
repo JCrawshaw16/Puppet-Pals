378b89ff92b68813a6b9e8dfd871b8850f3534bd
Marc Lyon <marcandlyon@gmail.com>
2016-11-30 11:56:58 +0000

    updated jenkins/nexus

--- shared/modules/jenkins/manifests/init.pp
+++ shared/modules/jenkins/manifests/init.pp
@@ -6,23 +6,20 @@ class jenkins ()
                 path => ["/usr/bin","/bin","/usr/sbin"]
         }
 		
-		exec {'create new jenkins directory' :
-				command => 'sudo mkdir /opt/jenkins',
-		}
+		
 		
 		  exec { 'copy jenkins' :
-                command => 'sudo cp /tmp/shared/jenkins_2.1_all.deb /opt/jenkins',
-				require => Exec['create new jenkins directory'],
-        }
+                command => 'sudo cp /tmp/shared/jenkins_2.1_all.deb /opt',
+		}
 		
-				    exec { 'install jenkins2' :
-                cwd => '/opt/jenkins',
-                command => 'sudo apt-get install -f',
+			exec { 'install jenkins2' :
+                cwd => '/opt',
+                command => 'sudo apt-get install -f -y',
                 require => Exec['copy jenkins'],
         }
 		
 		exec {'install jenkins1':
-				cwd => '/opt/jenkins',
+				cwd => '/opt',
                 command => 'sudo dpkg -i jenkins_2.1_all.deb',
 				require => Exec['install jenkins2'],
 		}
@@ -31,7 +28,7 @@ class jenkins ()
 
 		
 		    exec { 'install jenkins3' :
-                cwd => '/opt/jenkins',
+                cwd => '/opt',
                 command => 'sudo apt-get install -y jenkins',
                 require => Exec['install jenkins1'],
         }
--- shared/modules/nexus/manifests/init.pp
+++ shared/modules/nexus/manifests/init.pp
@@ -6,25 +6,20 @@ class nexus (
                 path => ["/usr/bin","/bin","/usr/sbin"]
         }
 
-      
-		
-		exec {'create new directory' :
-				command => 'sudo mkdir /opt/nexus'
-		}
+    
 		
 		  exec { 'copy nexus tar file' :
-                command => 'sudo cp /tmp/shared/nexus-3.0.2-02-unix.tar.gz /opt/nexus',
-				require => Exec['create new directory'],
+                command => 'sudo cp /tmp/shared/nexus-3.0.2-02-unix.tar.gz /opt',
         }
 
         exec { 'extract nexus tar file' :
-                cwd => '/opt/nexus',
+                cwd => '/opt',
                 command => 'sudo tar zxvf nexus-3.0.2-02-unix.tar.gz',
                 require => Exec['copy nexus tar file'],
         }
 
         exec { 'install nexus1' :
-				cwd => '/opt/nexus/nexus-3.0.2-02/bin',
+				cwd => '/opt/nexus-3.0.2-02/bin',
 				command => 'sudo ./nexus run &',
 				require => Exec['extract nexus tar file'],
         }
