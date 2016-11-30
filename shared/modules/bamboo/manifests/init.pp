b14438f3cd1957fa8e2db7fdb64cfe744a495559
Marc Lyon <marcandlyon@gmail.com>
2016-11-30 13:54:57 +0000

    modified bamboo

--- shared/modules/bamboo/manifests/init.pp
+++ shared/modules/bamboo/manifests/init.pp
@@ -3,29 +3,30 @@ class bamboo {
 	Exec {
 		path => ["/usr/bin","/bin","/usr/sbin",]
 	}
+	
+	#exec { 'add user':	
+	#	command => 'sudo useradd --create-home -c "Bamboo role account" bamboo'
+	#}
 
 	exec { 'make directory':	
 		command => 'sudo mkdir -p /opt/atlassian/bamboo',
-		require => Exec['add user'],
+		#require => Exec['add user'],
 	}
 
-	exec { 'ownership directory':
-		command => 'sudo chown bamboo: /opt/atlassian/bamboo',
-		require => Exec['make directory'],
-	}
+	#exec { 'ownership directory':
+	#	command => 'sudo chown bamboo: /opt/atlassian/bamboo',
+	#	require => Exec['make directory'],
+	#}
 
-	exec { 'add user':	
-		command => 'sudo useradd --create-home -c "Bamboo role account" bamboo'
-	}
 
-	exec { 'log in to user':
-		command => 'sudo su -bamboo',
-		require => Exec['add user'],
-	}
+	#exec { 'log in to user':
+	#	command => 'sudo su - bamboo',
+	#	require => Exec['add user'],
+	#}
 	
 	exec { 'copy bamboo tar file':
 		command => 'sudo cp /tmp/shared/atlassian-bamboo-5.14.1.tar.gz /opt/atlassian/bamboo',
-		require => Exec['make directory'],
+		#require => Exec['make directory'],
 	}
 
 	exec { 'extract bamboo tar file':
@@ -39,9 +40,15 @@ class bamboo {
 		command => 'sudo ln -s atlassian-bamboo-5.14.1/ current',
 		require => Exec['extract bamboo tar file'],
 	}
+	
+	#exec {'exit':
+	#	command => 'exit',
+	#	require => Exec['install bamboo'],
+	#}
 
 	exec { 'start up process':
 		cwd => '/opt/atlassian/bamboo/atlassian-bamboo-5.14.1/bin',
 		command => 'sudo ./start-bamboo.sh',
+		require => Exec['install bamboo'],
 	}
 }
